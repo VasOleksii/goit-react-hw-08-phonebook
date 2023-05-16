11be2db5d441a4cf56968164461121ed788853f1
VasOleksii <vasilenkooleksii@gmail.com>
2023-05-16 18:24:51 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'components/APP/App';
 import './index.css';
+import { Provider } from 'react-redux/es';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
