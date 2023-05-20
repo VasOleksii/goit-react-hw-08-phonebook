25e1863aee452c6f564ba74e8bb08144485d8e10
VasOleksii <111772928+VasOleksii@users.noreply.github.com>
2023-05-20 20:15:54 +0000

    Deploying to gh-pages from @ VasOleksii/goit-react-hw-08-phonebook@afd571276be42b663f2719447a4feaa1bba86b75 🚀

---
+++ static/js/305.32596d59.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[305],{4179:function(e,t,n){n.r(t),n.d(t,{NotFoundPage:function(){return s},default:function(){return l}});var o=n(1087),r="NotFoundPage_wrapper__gOFSd",a="NotFoundPage_title__VOkTX",u="NotFoundPage_link__+q4kT",c=n(3329),s=function(){return(0,c.jsxs)("div",{className:r,children:[(0,c.jsx)("h3",{className:a,children:"Sorry, page not found!"}),(0,c.jsx)(o.rU,{className:u,to:"/",children:"To Home"})]})},l=s}}]);
+//# sourceMappingURL=305.32596d59.chunk.js.map
---
+++ static/js/591.69a5f796.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[591],{591:function(e,o,t){t.r(o),t.d(o,{default:function(){return n}});var a="HomePage_wrapper__v-mkr",r=t(3329),n=function(){return(0,r.jsxs)("div",{className:a,children:[(0,r.jsx)("h1",{children:"Welcome to your Phonebook!"}),(0,r.jsx)("p",{children:"Welcome to Phone Book, this application will allow you to store private data and manage it with ease"})]})}}}]);
+//# sourceMappingURL=591.69a5f796.chunk.js.map
